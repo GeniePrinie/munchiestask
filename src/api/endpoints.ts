d39f64de0ff10d03d4b82babdeeff01feee3a030
genieprinie <geniexpn@gmail.com>
2024-11-21 11:39:58 +0100

    Add endpoints

---
+++ src/api/endpoints.ts
@@ -0,0 +1,11 @@
+export const API_BASE_URL =
+  "https://work-test-web-2024-eze6j4scpq-lz.a.run.app/api";
+
+export const ENDPOINTS = {
+  restaurants: `${API_BASE_URL}/restaurants`,
+  restaurant: `${API_BASE_URL}/restaurant/{id}`,
+  filters: `${API_BASE_URL}/filter`,
+  filter: `${API_BASE_URL}/filter/{id}`,
+  open: `${API_BASE_URL}/open/{id}`,
+  priceRange: `${API_BASE_URL}/price-range/{id}`,
+};
