0edf98fbfb4f812efcfb8c46e88d6639b646680b
genieprinie <geniexpn@gmail.com>
2024-11-21 11:45:34 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import RestaurantList from "./components/RestaurantList/RestaurantList";
+import Topbar from "./components/Layout/Topbar";
+import { DeliveryTimeRange, Filter } from "./types";
+import { getFilters } from "./api/services";
+import Sidebar from "./components/Layout/Sidebar";
 
 function App() {
+  const [filters, setFilters] = useState<Filter[]>([]);
+  const [activeFilters, setActiveFilters] = useState<string[]>([]);
+  const [activeDeliveryTimes, setActiveDeliveryTimes] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchFilters = async () => {
+      try {
+        const filtersData = await getFilters();
+        setFilters(filtersData);
+      } catch (error) {
+        console.error("App: Error fetching filters:", error);
+      }
+    };
+
+    fetchFilters();
+  }, []);
+
+  const handleFilterToggle = (filter: Filter) => {
+    setActiveFilters((prev) =>
+      prev.includes(filter.id)
+        ? prev.filter((id) => id !== filter.id)
+        : [...prev, filter.id]
+    );
+  };
+
+  const handleDeliveryTimeToggle = (timeRange: DeliveryTimeRange) => {
+    setActiveDeliveryTimes((prev) =>
+      prev.includes(timeRange.id)
+        ? prev.filter((id) => id !== timeRange.id)
+        : [...prev, timeRange.id]
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-gray-50 min-h-screen py-6 px-7">
+      <header className="flex items-center gap-2">
+        <div className="w-7">
+          <img src="/images/logo/munchiesLogo.png" alt="munchies logo" />
+        </div>
+        <h1 className="text-4xl">Munchies</h1>
       </header>
+
+      <div className="min-h-screen bg-gray-50 flex">
+        <div className="w-64 absolute left-8 top-20 h-screen">
+          <Sidebar
+            filters={filters}
+            activeFilters={activeFilters}
+            activeDeliveryTimes={activeDeliveryTimes}
+            onToggle={handleFilterToggle}
+            onDeliveryTimeToggle={handleDeliveryTimeToggle}
+          />
+        </div>
+
+        <div className="flex-1 ml-64">
+          <Topbar
+            filters={filters}
+            activeFilters={activeFilters}
+            onToggle={handleFilterToggle}
+          />
+
+          <main className="px-4 sm:px-6 lg:px-8 py-4">
+            <RestaurantList
+              activeFilters={activeFilters}
+              activeDeliveryTimes={activeDeliveryTimes}
+            />
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
